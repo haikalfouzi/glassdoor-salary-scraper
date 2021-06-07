5d95881a97e39bcd395824f4ff7fd1eaed771633
haikalfouzi <mrlock13@gmail.com>
2021-06-08 00:57:40 +0800

    Few errors need to be fixed. Glassdoor id/class name changes everytime..

--- scraper.py
+++ scraper.py
@@ -10,8 +10,6 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 
-username = "" # your email here
-password = "" # your password here
 
 def obj_dict(obj):
     return obj.__dict__
@@ -33,14 +31,12 @@ def login(driver, username, password):
     driver.get("http://www.glassdoor.com/profile/login_input.htm")
     try:
         user_field = driver.wait.until(EC.presence_of_element_located(
-            (By.NAME, "username")))
-        pw_field = driver.find_element_by_class_name("signin-password")
-        login_button = driver.find_element_by_id("signInBtn")
+            (By.ID, "userEmail")))
+        pw_field = driver.find_element_by_id("userPassword")
+        login_button = driver.find_element_by_name("submit")
         user_field.send_keys(username)
         user_field.send_keys(Keys.TAB)
-        time.sleep(1)
         pw_field.send_keys(password)
-        time.sleep(1)
         login_button.click()
     except TimeoutException:
         print("TimeoutException! Username/password field or login button not found on glassdoor.com")
@@ -50,8 +46,8 @@ def search(driver, city, title):
     driver.get("https://www.glassdoor.com/Salaries/index.htm")
     try:
         search_btn = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-            (By.CLASS_NAME, "showHH")))
-        time.sleep(5)
+            (By.ID, "HeroSearchButton")))
+        time.sleep(2)
         title_field = driver.find_element_by_id("KeywordSearch")
         city_field = driver.find_element_by_id("LocationSearch")
         title_field.send_keys(title)
@@ -69,10 +65,11 @@ def parse_salaries_HTML(salaries, data, city):
         jobTitle = "-"
         company = "-"
         meanPay = "-"
-        jobTitle = salary.find("a", { "class" : "jobTitle"}).getText().strip()
-        company = salary.find("div", { "class" : "i-emp"}).getText().strip()
+        jobTitle = salary.find('p',{'class':'m-0'}).text
+        company = salary.find('p',{'class':'m-0'}).next_sibling.text
         try:
-            meanPay = salary.find("div", { "class" : "meanPay"}).find("strong").getText().strip()
+            meanPay = salary.find('p',{'class':'d-block d-md-none m-0'}).text +\
+            " " + salary.find('p',{'class':'d-block d-md-none m-0 css-1kuy7z7'}).text
         except:
             meanPay = 'xxx'
         r = Salary.Salary(jobTitle, company, meanPay, city)
@@ -82,50 +79,53 @@ def parse_salaries_HTML(salaries, data, city):
 #enddef
 
 def get_data(driver, URL, city, data, refresh, startPage=1):
-    print "\nPage " + str(startPage)
+    print("\nPage " + str(startPage))
     if (refresh):
         driver.get(URL)
-        print "Getting " + URL
+        print( "Getting " + URL)
         time.sleep(2)
     try:
-        next_btn = driver.find_element_by_class_name("next")
+        next_btn = driver.find_element_by_class_name("pagination__ArrowStyle__nextArrow ")
         next_link = next_btn.find_element_by_css_selector("a").get_attribute('href')
     except:
         next_btn = False
         next_link = False
     #endif
-    time.sleep(2)
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "SalarySearchResults")))
     HTML = driver.page_source
     soup = BeautifulSoup(HTML, "html.parser")
     try:
-        salaries = soup.find("div", { "class" : ["salaryChartModule"] }).find_all("div", { "class" : ["salaryRow"] })
+        salaries = soup.find_all('div', {'data-test' : 'job-info'})
     except:
         salaries = False
     if (salaries):
         data = parse_salaries_HTML(salaries, data, city)
-        print "Page " + str(startPage) + " scraped."
+        print( "Page " + str(startPage) + " scraped.")
         if (next_link):
             get_data(driver, next_link, city, data, True, startPage + 1)
     else:
-        print "No data available for", city
+        print( "No data available for", city)
     #endif
     return data
-#enddef
+
 
 if __name__ == "__main__":
+
+    username = "gldoor01@maildrop.cc" # your email here
+    password = "bugmenot" # your password here
     driver = init_driver()
     time.sleep(3)
-    print "Logging into Glassdoor account ..."
+    print( "Logging into Glassdoor account ...")
     login(driver, username, password)
-    time.sleep(10)
     # search(driver, city, title)
-    print "\nStarting data scraping ..."
+    print( "\nStarting data scraping ...")
+    time.sleep(10)
     city_list = open("cities.txt").read().splitlines()
     data_out = []
     for city in city_list:
-        search(driver, city, 'Data Scientist')
+        search(driver, city, 'Mechanical Engineer')
         appendable = get_data(driver, driver.current_url, city, [], False, 1)
-        print "\nExporting data to " + city + ".json"
+        print( "\nExporting data to " + city + ".json")
         if appendable:
             data_out.append(appendable)
             json_export(appendable, city)
